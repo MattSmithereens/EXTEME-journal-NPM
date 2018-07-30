29ff4603c30e791456c946bd60e79baf107528e8
Matt Smith and Nate Cottle <matt+nate@>
2018-07-30 10:19:38 -0700

    javascript is hard, added journal functionality

---
+++ src/journal.js
@@ -0,0 +1,5 @@
+export function Entry(journal, body) {
+
+  this.journal = journal;
+  this.body = body;
+}
--- src/main.js
+++ src/main.js
@@ -1,5 +1,22 @@
-import { journal } from './journal';
+import { Entry } from './journal';
+import './styles.css';
+import $ from 'jquery';
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 $(document).ready(function(){
-  
-})
+  $('#thisForm').submit(function(e){
+    e.preventDefault();
+    //console.log($("#journal-title").val());
+    var journalTitle = $("#journal-title").val();
+    var body = $("#journal-body").val();
+    var userEntry = new Entry(journalTitle, body);
+    // userEntry.for(function(output) {
+    //       $('#journalEntry').append("<p>" + body + "</p>");
+    // });
+    $("#journalEntry").prepend(
+      "<h3>" + userEntry.journal + "</h3>",
+      "<p>" + userEntry.body + "</p>"
+    );
+  });
+});
